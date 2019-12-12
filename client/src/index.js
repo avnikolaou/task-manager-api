077b3bcd2ea8fcdd9d1202d500ce0ad84b3c1672
AvraamNikolaou <avnikolaou@hotmail.com>
2019-12-12 17:09:31 +0200

    fix: install libraries, connect to Redux console

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import { Provider } from 'react-redux'
+import {createStore, applyMiddleware, compose} from "redux";
+import reduxThunk from 'redux-thunk';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const store = createStore(() => [], {}, compose(applyMiddleware(reduxThunk), window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>, document.getElementById('root'));
