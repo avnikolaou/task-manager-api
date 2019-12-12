db937d9a690d2853c997ecbb0ad1d12765acdb1d
AvraamNikolaou <avnikolaou@hotmail.com>
2019-12-12 17:01:40 +0200

    feat: client initial setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <a href="/api/users/me">Click!</a>
+        </div>
+    );
+}
+
+export default App;
