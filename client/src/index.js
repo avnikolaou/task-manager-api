77e3a88ec824019fe4325afc12b507fc60c9eb00
AvraamNikolaou <avnikolaou@hotmail.com>
2020-01-28 16:41:55 +0200

    fix: redux dev-tools for production

--- client/src/index.js
+++ client/src/index.js
@@ -7,13 +7,13 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 // Redux
 import { Provider } from 'react-redux'
-import {createStore, applyMiddleware, compose} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import reduxThunk from 'redux-thunk';
 
 import combinereducers from './reducers';
 
 
-const store = createStore(combinereducers, {}, compose());
+const store = createStore(combinereducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
     <Provider store={store}><App /></Provider>, document.getElementById('root'));
